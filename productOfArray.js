75110c43361706b531b16795f0792bf35958914e
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2021-05-11 13:20:18 -0700

    add recursive solution

---
+++ productOfArray.js
@@ -0,0 +1,10 @@
+function productOfArray(numbers) {
+	if (numbers.length === 0) {
+		return 1
+	}
+	return numbers[0] * productOfArray(numbers.slice(1));
+}
+
+console.log(productOfArray([1]))
+console.log(productOfArray([1,2,3]))
+console.log(productOfArray([1,2,3,5,1]))
