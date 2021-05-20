4a7106bbe58d0b67a2f7dd7d184783d59e09b24e
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2021-05-20 14:41:22 -0700

    finish selection sort

---
+++ selectionSort.js
@@ -0,0 +1,18 @@
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[min] > arr[j]) {
+        min = j;
+      }
+    }
+    if (i !== min) {
+      temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+  }
+  return arr;
+};
+
+console.log(selectionSort([2, -3, 0, 3, 1]));
